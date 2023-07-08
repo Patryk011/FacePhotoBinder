dcf3cf21fe3161d6157405627b928fc1128d8909
Patryk <patryknartowski@wp.pl>
2023-07-08 22:12:44 +0200

    update vue config2

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   devServer: {
+    port: process.env.PORT || 8080,
+    host: "0.0.0.0",
     allowedHosts: "all",
     headers: {
-      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin": "https://photo-binder.onrender.com",
     },
   },
   transpileDependencies: true,
